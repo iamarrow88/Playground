a930bb0e89dd5ebd0072acfa16f879d9c9c93eb2
Nana Pisanko <bonjozzy@gmail.com>
2022-10-09 20:13:03 +0300

    feat: 4. add services

--- Tour-of-Heroes-App/src/app/heroes/heroes.component.ts
+++ Tour-of-Heroes-App/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from './../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService} from "../hero.service";
+import { MessageService } from "../messages.service";
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +10,26 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
-  constructor() { }
+  heroClicked: number = 0;
+  constructor(private heroService: HeroService ,private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHerose();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  }
+    this.heroClicked++;
+    this.messageService.add(this.heroClicked.toString());
+    this.messageService.add(hero.name);
 
+  }
+  getHerose(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+  countClicks(): void {
+    this.heroClicked++;
+  }
 }
