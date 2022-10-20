f7f1343f830207ba88528ecfdaa439c25d6cd8b4
Nana Pisanko <bonjozzy@gmail.com>
2022-10-20 15:17:09 +0300

    feat: implement registration form HTML template

--- Tour-of-Heroes-App/youtube-client/youtube-client/src/app/app.module.ts
+++ Tour-of-Heroes-App/youtube-client/youtube-client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { CreateCardFormComponent } from './create-card-form/create-card-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegistrationFormComponent,
+    CreateCardFormComponent
   ],
   imports: [
     BrowserModule,
