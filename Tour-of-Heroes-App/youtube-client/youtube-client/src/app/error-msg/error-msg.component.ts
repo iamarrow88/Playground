6a5c2fdbbda313836e04497f85065072a67e196f
Nana Pisanko <bonjozzy@gmail.com>
2022-10-20 21:05:34 +0300

    feat: implement error html template

---
+++ Tour-of-Heroes-App/youtube-client/youtube-client/src/app/error-msg/error-msg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-msg',
+  templateUrl: './error-msg.component.html',
+  styleUrls: ['./error-msg.component.scss']
+})
+export class ErrorMsgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
