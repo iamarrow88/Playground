324787d4999aa5ea1bd80510f17c14ce9c058c11
Nana Pisanko <bonjozzy@gmail.com>
2022-10-20 15:35:00 +0300

    feat: implement create card HTML template

---
+++ Tour-of-Heroes-App/youtube-client/youtube-client/src/app/create-card-form/create-card-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-card-form',
+  templateUrl: './create-card-form.component.html',
+  styleUrls: ['./create-card-form.component.scss']
+})
+export class CreateCardFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
