53ba0e45020d5579c1a6aa7b7e0fc81681e85222
Nana Pisanko <bonjozzy@gmail.com>
2022-12-08 19:22:26 +0300

    init: initial commit for nodejs branch

--- angular-messenger/src/mocks/current-user.mock.ts
+++
@@ -1,7 +0,0 @@
-import { UserModel } from '../models/user.model';
-
-export const currentUserMock: UserModel = {
-  username: 'Steve Bangalter',
-  last_seen: 'online',
-  id: '62154955353e6c41ea0348ee',
-};
--- angular-messenger/src/models/user.model.ts
+++
@@ -1,5 +0,0 @@
-export interface UserModel {
-  username: string;
-  last_seen: string; // online or last seen date
-  id: string;
-}
