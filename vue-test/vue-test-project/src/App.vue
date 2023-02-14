b6d82d16fe9fdb90d7ba4a58fd38be97b19cb5cf
Nana Pisanko <bonjozzy@gmail.com>
2023-02-14 10:42:14 +0300

    feat: first simple counter add

---
+++ vue-test/vue-test-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Hello world!</h1>
+  <div>Likes: <strong> {{likes}}</strong></div>
+  <div>Dislikes: <strong> {{dislikes}}</strong></div>
+  <button @click="addLike">like!</button>
+  <button @click="addDislike">Dislike!</button>
+</template>
+
+<script>
+  export default { //скрипт всегда содержит export default и возвращает объект
+    data() { //функция дата содержит (возвращает) модели
+      return {
+        likes: 0, // модель
+        dislikes: 0
+      }
+    },
+    methods: { //объект методы содержит методы компонента (без function)
+      addLike() {
+        return this.likes += 1;
+      },
+
+      addDislike(){
+        this.dislikes += 1;
+      }
+    }
+  }
+</script>
+
+<style> /*содержит стили компонента*/
+
+</style>
