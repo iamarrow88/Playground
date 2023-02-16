e677755eb748616a48b48415c6f422b1ad276219
Nana Pisanko <bonjozzy@gmail.com>
2023-02-16 10:34:44 +0300

    feat: animations

--- vue-test/vue-test-project/src/components/postList.vue
+++ vue-test/vue-test-project/src/components/postList.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="post"
-       v-for="post in posts"
-       :id="post.id"
-       :key="post.id">
-    <post-item :post="post"></post-item>
-    <div class="post-btns">
-      <my-button @click="$emit('remove', post)">Delete</my-button>
+  <transition-group name="posts-list"> <!--1.обернуть нужный элемент в спец тег (см документацию)-->
+    <div class="post"
+         v-for="post in posts"
+         :id="post.id"
+         :key="post.id">
+      <post-item :post="post"></post-item>
+      <div class="post-btns">
+        <my-button @click="$emit('remove', post)">Delete</my-button>
+      </div>
     </div>
-  </div>
+  </transition-group>
 </template>
 
 <script>
@@ -39,5 +41,23 @@ export default {
   align-items: center;
   border: 2px solid teal;
   max-width: 1000px;
+}
+/*2 добавить спец стили (см документацию. первая часть названия класса - это name из спец тега,
+ вторая часть - специальное фиксированное название*/
+.posts-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.posts-list-enter-active,
+.posts-list-leave-active {
+  transition: all 1s ease;
+}
+.posts-list-enter-from,
+.posts-list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.posts-list-move {
+  transition: transform 0.8s ease;
 }
 </style>
