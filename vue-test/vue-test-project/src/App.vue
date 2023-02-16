ebf730c19dc21c2dbb25ac1d59d192493887ca0c
Nana Pisanko <bonjozzy@gmail.com>
2023-02-16 11:00:36 +0300

    feat: search

--- vue-test/vue-test-project/src/App.vue
+++ vue-test/vue-test-project/src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <h1>JS Blog</h1>
   <div class="container">
-  <my-dialog @changeShown="shown=false" v-if="shown">
-    <post-form @create="addPost"></post-form>
-  </my-dialog>
-  <div class="add-post">
-    <my-button @click="shown=true">Add Post</my-button>
-    <my-select :options="options" v-model="selectedSort"></my-select>
-  </div>
-    <div v-if="!isPostsLoading">
-      <post-list v-if="posts.length > 0" :posts=sortedPosts @remove="removePost"></post-list>
+    <my-dialog @changeShown="shown=false" v-if="shown">
+      <post-form @create="addPost"></post-form>
+    </my-dialog>
+    <my-input v-model:value="keyToSearch"
+              placeholder="Search..." /> <!--1. поиск. добавляем в инпут
+              v-model:value="keyToSearch" - модель, где будет храниться
+              ключевое слово для поиска-->
+    <div class="add-post">
+      <my-button @click="shown=true">Add Post</my-button>
+      <my-select :options="options" v-model="selectedSort"></my-select>
+    </div>
+    <div v-if="!isPostsLoading"> <!--3. этот результат передаем в дочерний элемент
+                                  для отрисовки-->
+      <post-list v-if="posts.length > 0" :posts=searchedAndSortedPosts @remove="removePost"></post-list>
       <div v-else>
         <h3>Nothing to read</h3>
       </div>
@@ -17,7 +22,6 @@
     <div v-else>
       Posts are loading...
     </div>
-
   </div>
 </template>
 
@@ -46,7 +50,8 @@ import MySelect from "./components/UI/MySelect.vue";
           {name: 'By Title', value: 'title', id: 0},
           {name: 'By Article', value: 'body', id: 1},
         ],
-        selectedSort: 'title'
+        selectedSort: 'title',
+        keyToSearch: '' //2. определяем ее в компоненте
       }
     },
     methods: {
@@ -75,6 +80,10 @@ import MySelect from "./components/UI/MySelect.vue";
     computed: {
       sortedPosts() {
         return [...this.posts].sort((post1, post2) => post1[this.selectedSort].localeCompare(post2[this.selectedSort]))
+      },
+      searchedAndSortedPosts(){ //3. также в компьютед описываем функцию, которая
+        //будет что-то делать в массивом и возвращать результат
+        return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.keyToSearch.toLowerCase()))
       }
     }
   }
